1fab6f976a745194882563ac126cf68f7e1de4d9
Jleev77 <jleev77@gmail.com>
2017-03-28 19:30:28 -0400

    Initial Checkin
    Initial Checkion

---
+++ Assets/DataFramework/ILocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.DataFramework
+{
+    interface ILocation : IComponent
+    {
+        float X { get; set; }
+        float Y { get; set; }
+    }
+}
